91d029b932e64e806467d13e1b403f05b0df99a8
Diana <kabadiana327@gmail.com>
2022-09-18 14:26:50 +0300

    Exercise so far at 11

---
+++ script.js
@@ -0,0 +1,25 @@
+let n = 13;
+while (n > 0) {
+  alert("Your turn!");
+  let m = prompt("Get the matches!");
+  if (isNaN(m)) {
+    alert("Not a Number");
+    continue;
+  } else if (m < 0 || m > 3) {
+    continue;
+  }
+  n -= m;
+  alert(n);
+  if (n <= 0) {
+    alert("Your win!");
+    break;
+  }
+  alert("My turn!");
+  let random = Math.floor(Math.random() * 3) + 1;
+  let j = prompt("Got the matches!");
+  random -= j;
+  if (j <= 0) {
+    alert("I win!");
+    break;
+  }
+}

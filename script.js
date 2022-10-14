3f76d530ff6afac094b745fa125005c47242d3b7
Diana <kabadiana327@gmail.com>
2022-10-14 15:40:32 +0300

    matches with function

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-let n = 13;
-while (n > 0) {
-  alert("Your turn!");
-  let m = prompt("Get the matches!");
-  if (isNaN(m)) {
-    alert("Not a Number");
-    continue;
-  } else if (m < 0 || m > 3) {
-    continue;
-  }
-  n -= m;
-  alert(n);
-  if (n <= 0) {
-    alert("Your win!");
-    break;
-  }
-  alert("My turn!");
-  let random = Math.floor(Math.random() * 3) + 1;
-  n -= random;
-  let j = prompt("Got the matches!");
-  random -= j;
-  if (j <= 0) {
-    alert("I win!");
-    break;
+function matches(count) {
+  count = 13;
+  while (count > 0) {
+    alert("Your turn!");
+    let m = prompt("Get the matches!");
+    if (isNaN(m)) {
+      alert("Not a Number");
+      continue;
+    } else if (m < 0 || m > 3) {
+      continue;
+    }
+    count -= m;
+    alert(count);
+    if (count <= 0) {
+      alert("Your win!");
+      break;
+    }
+    alert("My turn!");
+    let random = Math.floor(Math.random() * 3) + 1;
+    count -= random;
+    let j = prompt("Got the matches!");
+    random -= j;
+    if (j <= 0) {
+      alert("I win!");
+      break;
+    }
   }
 }
+
+matches();
